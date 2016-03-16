9f074880ef81f3d805317872bceb7151f5d63a10
Diego Fernández Barrera <bigomby@gmail.com>
2016-03-16 15:10:56 +0100

    Added core package files

---
+++ backend.go
@@ -0,0 +1,164 @@
+package rbforwarder
+
+import "os"
+
+// Listener is the component that gets data from a source, then sends the data
+// to the backend.
+type Listener interface {
+	Listen(Forwarder)
+	Close()
+}
+
+// Decoder is the component that parses a raw buffer to a structure
+type Decoder interface {
+	Init(int) error
+	Decode(*Message) error
+}
+
+// Processor performs operations on a data structure
+type Processor interface {
+	Init(int) error
+	Process(message *Message) (bool, error)
+}
+
+// Encoder serializes a data structure to a output buffer
+type Encoder interface {
+	Init(int) error
+	Encode(*Message) error
+}
+
+// Sender takes a raw buffer and sent it using a network protocol
+type Sender interface {
+	Init(int) error
+	Send(*Message) error
+}
+
+type backend struct {
+	listener  Listener
+	decoder   Decoder
+	processor Processor
+	encoder   Encoder
+	sender    Sender
+
+	// Pool of workers
+	decoderPool   chan chan *Message
+	processorPool chan chan *Message
+	encoderPool   chan chan *Message
+	senderPool    chan chan *Message
+
+	currentProducedID  int64
+	currentProcessedID int64
+
+	messages chan *Message
+	reports  chan Report
+
+	messagePool chan *Message
+}
+
+// Worker that decodes the received message
+func (b *backend) startDecoder(i int) {
+	if b.decoder != nil {
+		b.decoder.Init(i)
+	}
+	workerChannel := make(chan *Message)
+
+	go func() {
+		for {
+			// The worker is ready, put himself on the worker pool
+			b.decoderPool <- workerChannel
+
+			// Wait for a new message
+			message := <-workerChannel
+
+			// Perform work on the message
+			if b.decoder != nil {
+				b.decoder.Decode(message)
+			}
+
+			// Get a worker for the next element on the pipe
+			messageChannel := <-b.processorPool
+
+			// Send the message to the next worker
+			messageChannel <- message
+		}
+	}()
+}
+
+// Worker that performs modifications on a decoded message
+func (b *backend) startProcessor(i int) {
+	if b.processor != nil {
+		b.processor.Init(i)
+	}
+	workerChannel := make(chan *Message)
+
+	// The worker is ready, put himself on the worker pool
+	go func() {
+		for {
+			// The worker is ready, put himself on the worker pool
+			b.processorPool <- workerChannel
+
+			// Wait for a new message
+			message := <-workerChannel
+
+			// Perform work on the message
+			if b.processor != nil {
+				b.processor.Process(message)
+			}
+
+			// Get a worker for the next element on the pipe
+			messageChannel := <-b.encoderPool
+
+			// Send the message to the next worker
+			messageChannel <- message
+		}
+	}()
+}
+
+// Worker that encodes a modified message
+func (b *backend) startEncoder(i int) {
+	if b.encoder != nil {
+		b.encoder.Init(i)
+	}
+	workerChannel := make(chan *Message)
+
+	go func() {
+		for {
+			// The worker is ready, put himself on the worker pool
+			b.encoderPool <- workerChannel
+
+			// Wait for a new message
+			message := <-workerChannel
+
+			// Perform work on the message
+			if b.encoder != nil {
+				b.encoder.Encode(message)
+			} else {
+				message.OutputBuffer = message.InputBuffer
+			}
+
+			// Get a worker for the next element on the pipe
+			messageChannel := <-b.senderPool
+
+			// Send the message to the next worker
+			messageChannel <- message
+		}
+	}()
+}
+
+// Worker that sends the message
+func (b *backend) startSender(i int) {
+	if b.sender == nil {
+		os.Exit(1)
+	}
+
+	b.sender.Init(i)
+	workerChannel := make(chan *Message)
+
+	go func() {
+		for {
+			b.senderPool <- workerChannel
+			message := <-workerChannel
+			b.sender.Send(message)
+		}
+	}()
+}
---
+++ messages.go
@@ -0,0 +1,96 @@
+package rbforwarder
+
+import (
+	"bytes"
+	"errors"
+	"time"
+)
+
+const (
+	statusOk = 0
+)
+
+// Message is used to send data to the backend
+type Message struct {
+	InputBuffer  *bytes.Buffer          // The original data from the listener
+	Data         interface{}            // Can be used to store the data once it has been parsed
+	OutputBuffer *bytes.Buffer          // The data that will be sent by the sender
+	Metadata     map[string]interface{} // Opaque
+
+	retries int      // Number of retries of this message
+	backend *backend // Use to send the message to the backend
+}
+
+// Produce is used by the listener to send messages to the backend
+func (m *Message) Produce() error {
+	select {
+	case messageChannel := <-m.backend.decoderPool:
+		select {
+		case messageChannel <- m:
+		case <-time.After(1 * time.Second):
+			return errors.New("Error on produce: Full queue")
+		}
+	case <-time.After(1 * time.Second):
+		return errors.New("Error on produce: No workers available")
+	}
+
+	return nil
+}
+
+// Ack is used by the sender acknowledge a message
+func (m *Message) Ack(status string) error {
+	report := Report{
+		ID:         m.backend.currentProducedID,
+		StatusCode: statusOk,
+		Status:     status,
+		Metadata:   m.Metadata,
+
+		message: m,
+	}
+
+	select {
+	case m.backend.reports <- report:
+		m.backend.currentProducedID++
+	case <-time.After(1 * time.Second):
+		return errors.New("Error on report: Full queue")
+	}
+
+	return nil
+}
+
+// Fail is used by the sender to inform that a message has not been sent
+func (m *Message) Fail(statusCode int, status string) error {
+
+	// If statusCode is zero, it's actually an Ack
+	if statusCode == 0 {
+		return m.Ack(status)
+	}
+
+	report := Report{
+		ID:         m.backend.currentProducedID,
+		StatusCode: statusCode,
+		Status:     status,
+		Metadata:   m.Metadata,
+
+		message: m,
+	}
+
+	select {
+	case m.backend.reports <- report:
+		m.backend.currentProducedID++
+	case <-time.After(1 * time.Second):
+		return errors.New("Error on report: Full queue")
+	}
+
+	return nil
+}
+
+// Report is used by the listener to obtain the status of a sent message
+type Report struct {
+	ID         int64                  // Unique ID for the report, used to maintain sequence
+	Status     string                 // Result of the sending
+	StatusCode int                    // Result of the sending
+	Metadata   map[string]interface{} // Opaque
+
+	message *Message
+}
