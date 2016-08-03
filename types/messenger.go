c42674cabb0572499c84b71da71c059eff32fc04
Diego Fernández Barrera <bigomby@gmail.com>
2016-08-03 14:48:13 +0200

    :sparkles: Remove PushData() and add PopOpts() methods
    Closes #16

--- types/messenger.go
+++ types/messenger.go
@@ -3,5 +3,5 @@ package types
 // Messenger is used by modules to handle messages
 type Messenger interface {
 	PopData() ([]byte, error)
-	PushData(data []byte)
+	PopOpts() (map[string]interface{}, error)
 }
